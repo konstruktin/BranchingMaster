0cf76fd7ef0a289f4673c1db55d2e64475580ef9
konstruktin <konstruktin@gmail.com>
2019-06-21 15:59:28 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Importance value = Importance.Critical;
+
+            if (value == Importance.Trivial)
+            {
+                Console.WriteLine("Not true");
+            }
+
+            Singleton mySingleton = Singleton.Instance;
+            Singleton mySingleton2 = Singleton.Instance;
+        }
+    }
+}
